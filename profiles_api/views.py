5302b1c49b521a95a20b3622f3a9d61d27bc10fc
Crhistian Mendoza <crhistianmendoza5@gmail.com>
2021-03-02 11:50:13 -0500

    creada view de prueba, creada la url de de profiles_app, incluida el url de la app dentro de url del proyecto

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """API view de prueba"""
+    def get(self, request,format=None):
+        """retornar lista de caracteristicas del APIview"""
+        an_apiview=[
+            'usamos metodos http como funciones (get, post, patch, put, delete)',
+            'es similar a una vista tradicional de django',
+            'nos da el amyor control sobre la logica de nuestra aplicacion',
+            'esta mapeado manuealmente los urls',
+        ]
+        return Response({'message':'hello','an_apiview':an_apiview})
